7163ed3298ade164a0adbee748522d66598d6560
Ayush-Juyal <78803826+Ayush-Juyal@users.noreply.github.com>
2022-07-15 00:14:11 +0530

    Add files via upload

---
+++ 1_1_7_variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    x=10;
+    cout<<x<<" ";
+    int y=20;
+    cout<<y<<" ";
+    return 0;
+}
---
+++ 1_1_9_data_types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=10;
+    string name="arjun";
+    char gender='m'; 
+    bool ismarried=true;
+    float weight=68.5;
+    cout<<name<<"\n"
+        <<age<<"\n"
+        <<gender<<"\n"
+        <<ismarried<<"\n"
+        <<weight;
+        return 0;
+
+
+}
